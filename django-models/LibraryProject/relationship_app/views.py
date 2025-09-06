2b1729ee72293ab81f8e64513bd0711fb10373e3
hamazin <hademolahamazin@gmail.com>
2025-09-06 03:45:51 +0100

    auth login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.shortcuts import redirect
+from django.contrib.auth import login
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
