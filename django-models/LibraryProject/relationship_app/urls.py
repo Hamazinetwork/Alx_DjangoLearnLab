6d90d82c1feeec2b59a3471b53213d65c3417235
hamazin <hademolahamazin@gmail.com>
2025-09-06 03:48:39 +0100

    authentication route

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,15 @@
+# relationship_app/urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView  
-from django.contrib.auth import views as auth_views
-from .views import RegisterView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView, RegisterView
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("register/", RegisterView.as_view(), name="register"),
-]
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 
-urlpatterns = [
-    path("books/", list_books, name="list_books"), 
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    #  Authentication routes
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
