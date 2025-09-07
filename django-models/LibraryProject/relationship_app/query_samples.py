99b1ae336b1912208fd48a51dba615ea12902390
hamazin <hademolahamazin@gmail.com>
2025-09-07 23:22:58 +0100

    querysample update

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,14 +3,15 @@ from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author_name = "George Orwell"
-books_by_author = Book.objects.filter(author__name=author_name)
+author = Author.objects.get(name=author_name)   
+books_by_author = Book.objects.filter(author=author)  
 print(f"Books by {author_name}:")
 for book in books_by_author:
     print(book.title)
 
 # List all books in a library
 library_name = "Central Library"
-library = Library.objects.get(name=library_name)  
+library = Library.objects.get(name=library_name)
 print(f"Books in {library_name}:")
 for book in library.books.all():
     print(book.title)
