82e48850115894ad7a3ba125e20d8f1e8f7fe9db
hamazin <hademolahamazin@gmail.com>
2025-09-14 22:43:11 +0100

    authusermodel

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -111,3 +111,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# advanced_features_and_security/settings.py
+AUTH_USER_MODEL = "bookshelf.CustomUser"
