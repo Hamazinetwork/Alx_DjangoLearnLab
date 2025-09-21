75cd36c939ce289549c179dfa2b9a156269d3290
hamazin <hademolahamazin@gmail.com>
2025-09-21 22:55:23 +0100

    modeltoBook serializer

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import MyModel
-from .serializers import MyModelSerializer
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListCreateAPIView):
-    queryset = MyModel.object.all()
-    serializer_class = MyModelSerializer
+    queryset = Book.object.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        name_filter = self.request.query_params.get('name', None)
+        if name_filter is not None:
+            queryset = queryset.filter(name__icontains=name_filter)
+        return queryset
