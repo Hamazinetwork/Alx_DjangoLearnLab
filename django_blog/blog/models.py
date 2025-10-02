75a7394b9362cc81fdbe45dfe40c2a8597064cf8
hamazin <hademolahamazin@gmail.com>
2025-10-02 02:41:58 +0100

    taggit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.utils import timezone
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    tags = TaggableManager(blank=True)  
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.sites',
+    'taggit',
     'blog',
     "USER",
     "PORT",
