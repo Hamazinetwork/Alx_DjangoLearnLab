34bf5e608a61fce724491030c83b1a6ccbc681fe
hamazin <hademolahamazin@gmail.com>
2025-10-02 02:27:13 +0100

    added comment to model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.utils import timezone
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment(by={self.author.username} on post={self.post.id})"
 
 
 class Profile(models.Model):
